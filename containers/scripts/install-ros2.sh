b51b08ebd187e9b7fbe9e7eac391906291aefc83
Ishaan Datta <ishaandatta737@gmail.com>
2024-11-20 23:06:18 -0800

    chanhge for script

--- containers/scripts/install-ros2.sh
+++ containers/scripts/install-ros2.sh
@@ -14,7 +14,7 @@ apt-get install -y --no-install-recommends \
 	locales \
 	software-properties-common
 
-add-apt-repository universe
+add-apt-repository universe -y
 apt-get update
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 # echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
